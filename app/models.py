c0ab98cdc8483520aa2485cac953856d1c960cf8
Pablo Pederiva da Cruz <pablopederiva@hotmail.com>
2023-02-12 21:23:56 -0300

    nullable to true

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="TRUE", nullable=False)
     created_at = Column(
-        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=True
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
 
 
@@ -24,5 +24,5 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)  # No duplicates
     password = Column(String, nullable=False)
     created_at = Column(
-        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=True
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
